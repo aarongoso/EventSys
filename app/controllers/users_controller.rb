511b6bc5de67712ea533f42494cca41bfa8d74cd
aaron <x22513383@student.ncirl.ie>
2025-12-04 19:20:00 +0000

    CI/CD fix: removed :role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through
     def user_params
-      params.require(:user).permit(:name, :email, :role)
+      params.require(:user).permit(:name, :email) # removed role to pass CI
     end
 end
